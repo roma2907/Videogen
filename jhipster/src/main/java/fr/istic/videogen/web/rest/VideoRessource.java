b4da52887d3f3840444b695a08ca066d09879722
ramage <ramageromain@gmail.com>
2016-01-07 20:39:08 +0100

    correctif

--- jhipster/src/main/java/fr/istic/videogen/web/rest/VideoRessource.java
+++ jhipster/src/main/java/fr/istic/videogen/web/rest/VideoRessource.java
@@ -1,9 +1,14 @@
 package fr.istic.videogen.web.rest;
 
+import java.io.FileNotFoundException;
+import java.net.URI;
+import java.net.URL;
+
 import javax.servlet.ServletContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,13 +22,12 @@ import fr.istic.videogen.playlistFormat.TypeGenerator;
 @RestController
 @RequestMapping(value = "/api/videos", produces = MediaType.APPLICATION_JSON_VALUE)
 public class VideoRessource {
-	
-	@Autowired ServletContext context;
-	
-	 @RequestMapping(method = RequestMethod.GET)
-	    public String getAll() {
-	       String url = Generator.createVideo("src/main/webapp/resources/test.videogen",TypeGenerator.M3UEXT,"src/main/webapp/resources/file.m3u8");
-	       
-	       return "{\"url\":\""+url+"\"}";
+
+	@RequestMapping(method = RequestMethod.GET)
+	    public String getAll() throws FileNotFoundException  {
+			URL url = ResourceUtils.getURL("src/main/webapp/resources/test.videogen");
+			String file = Generator.createVideo(url,TypeGenerator.M3UEXT,"src/main/webapp/resources/file.m3u8");
+			System.out.println(file);
+			return "{\"url\":\""+file+"\"}";
 	    }
 }
