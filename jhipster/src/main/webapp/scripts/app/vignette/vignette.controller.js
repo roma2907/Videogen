61465d1777f3ffda78278c420ce88f051354512e
Tanguy Bernard <bernardtanguy1pro@gmail.com>
2016-01-12 18:06:36 +0100

    vignette videogen file good

--- jhipster/src/main/webapp/scripts/app/vignette/vignette.controller.js
+++ jhipster/src/main/webapp/scripts/app/vignette/vignette.controller.js
@@ -7,4 +7,22 @@ angular.module('generatorVideoApp')
                 $scope.vignettes = v;
     	    });
 
+        $scope.items = [
+            {imageUrl: '/app/img/item1.jpg'},
+            {imageUrl: '/app/img/item2.jpg'}
+        ];
+        $scope.selectedItem = null;
+
+        $scope.selectItem = function(item){
+            console.log(item);
+            $scope.selectedItem = item;
+        };
+
+        $scope.isSelected = function(item){
+            if($scope.selectedItem===null){
+                return false;
+            }
+            return item.imageUrl === $scope.selectedItem.imageUrl;
+        };
+
     });
